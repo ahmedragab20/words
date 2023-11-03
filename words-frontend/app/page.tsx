f7af7108bf984e438920926098dd6b7fe1ed4fe7
ahmedragab <ahmedragab20901@gmail.com>
2023-11-03 02:05:51 +0200

    init the app

---
+++ words-frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-7xl capitalize font-serif">
+      <div className="py-2 px-3 border-2">hello next.js</div>
+    </div>
+  );
+}
